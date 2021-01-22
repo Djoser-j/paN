aee3c7dc7d23c38dbd6f445d909af0337ecab0d0
Djoser-j <73600992+Djoser-j@users.noreply.github.com>
2021-01-22 17:03:18 +0100

    Add files via upload

--- library/more/clear.bat
+++ library/more/clear.bat
@@ -1,1 +1,27 @@
-nothing here
+@echo off
+:: ---------------------------------------------------------------
+:: Delete compiles and logfiles
+
+:: To be run in the paN base directory
+:: ---------------------------------------------------------------
+
+:: Library name
+set name=pan_arith
+
+:: working directory
+set work=.\workdir
+:: library directory
+set ldir=.\library
+:: source files directory
+set tool=.\modules\bin
+
+echo.
+
+del %ldir%\lib%name%.dll.a
+del %tool%\%name%.dll
+
+for %%a in (%tool%\*.exe) do del "%%~fa" >nul
+
+for %%a in (%work%\*.log) do del "%%~fa" >nul
+
+exit
